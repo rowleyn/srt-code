c039d414e7b81c7278e405a2649245c06dfe53a4
rowleyn <rowleyn@carleton.edu>
2018-06-20 13:22:25 -0500

    Mostly finished Scan.py, just need to deal with time formatting

--- backend/CommandStation.py
+++ backend/CommandStation.py
@@ -3,6 +3,7 @@ A class that uses a TCP socket to command the station
 through an Ethernet to serial converter.
 
 Author: Nathan Rowley
+Date: June 2018
 '''
 
 import socket
@@ -13,6 +14,7 @@ from astropy.time import Time
 import ntplib
 
 ### change these depending on which converter is attached to the telescope and how it is configured ###
+
 SEND_PORT = 23 				# port number (int) of the converter connected to the station
 SEND_IP = '192.168.0.8' 	# ip address (str) of the converter connnected to the station
 DEFAULT_BUFFER_SIZE = 4096
@@ -26,9 +28,10 @@ class CommandStation:
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.sock.settimeout(DEFAULT_TIMEOUT)
 
-	#######################################################
+	
 	'''
 	Method that commands the station to move to a particular azimuth and altitude.
+
 	:param az: target azimuth
 	:param al: target altitude
 	:return:
@@ -37,16 +40,16 @@ class CommandStation:
 
 		### get current station position ###
 
-		with open('srtconfig.json') as f:				#get station data from config
+		with open('srtconfig.json') as f:				# get station data from config
 			station = json.load(f)
-		curaz = station['config']['azal']['azimuth']	#get current azimuth
-		cural = station['config']['azal']['altitude']	#get current altitude
+		curaz = station['config']['azal']['azimuth']	# get current azimuth
+		cural = station['config']['azal']['altitude']	# get current altitude
 
 		print(str(curaz)+' '+str(cural))
 
 		azset = False
 		alset = False
-		if curaz == az:		#check to make sure station needs to move
+		if curaz == az:		# check to make sure station needs to move
 			azset = True
 		if cural == al:
 			alset = True
@@ -54,45 +57,45 @@ class CommandStation:
 		### move station to correct azimuth and altitude ###
 		
 		if not azset:
-			if (az - curaz) > 0:	#determine azimuth increase (direction = 1) or decrease (direction = 0)
+			if (az - curaz) > 0:	# determine azimuth increase (direction = 1) or decrease (direction = 0)
 				direction = 1
 			else:
 				direction = 0
-			azcount = int(round(abs(az - curaz)*11.7))	#determine azimuth count value for the stamp controller
+			azcount = int(round(abs(az - curaz)*11.7))	# determine azimuth count value for the stamp controller
 		elif not alset:
-			if (al - cural > 0):	#determine altitude increase (direction = 3) or decrease (direction = 2)
+			if (al - cural > 0):	# determine altitude increase (direction = 3) or decrease (direction = 2)
 				direction = 3
 			else:
 				direction = 2
-			alcount = int(round(abs(al - cural)*11.7)) 	#determine altitude count value
+			alcount = int(round(abs(al - cural)*11.7)) 	# calculate altitude count value
 
-		self.sock.connect((self.addr,self.port))		#open connection to the serial to ethernet converter
+		self.sock.connect((self.addr,self.port))		# open connection to the serial to ethernet converter
 
-		while (not azset or not alset):					#while loop will always set azimuth first if not already set, then altitude if not already set
+		while (not azset or not alset):					# while loop will always set azimuth first if not already set, then altitude if not already set
 
 			if not azset:
-				message = ' move ' + str(direction) + ' ' + str(azcount) + '\n'	#move to new azimuth
+				message = ' move ' + str(direction) + ' ' + str(azcount) + '\n'	# move to new azimuth
 			elif not alset:
-				message = ' move ' + str(direction) + ' ' + str(alcount) + '\n'	#move to new altitude
+				message = ' move ' + str(direction) + ' ' + str(alcount) + '\n'	# move to new altitude
 			print(message)
-			self.sock.send(message.encode('ascii'))					#send message to stamp via serial to ethernet converter
+			self.sock.send(message.encode('ascii'))					# send message to stamp via serial to ethernet converter
 			print('data sent')
 			
-			response = self.sock.recv(DEFAULT_BUFFER_SIZE).decode('ascii').strip().split()		#receive and format response data
+			response = self.sock.recv(DEFAULT_BUFFER_SIZE).decode('ascii').strip().split()		# receive and format response data
 			print('reply received')
 			
-			if response[0] == 'M':						#response indicates successful completion of movement
+			if response[0] == 'M':						# response indicates successful completion of movement
 				if not azset:
 					azset = True
 				elif not alset:
 					alset = True
-			else:										#response indicates stamp timeout, must resend message with updated values
-				if response[1] != response[3]:			#update count based on returned value
+			else:										# response indicates stamp timeout, must resend message with updated values
+				if response[1] != response[3]:			# update count based on returned value
 					if not azset:
 						azcount = azcount - int(response[1])
 					elif not alset:
 						alcount = alcount - int(response[1])
-				else:									#if target is reached despite timeout
+				else:											# if target is reached despite timeout
 					if not azset:
 						azset = True
 					elif not alset:
@@ -100,23 +103,24 @@ class CommandStation:
 
 		### update current station position ###
 
-		station['config']['azal']['azimuth'] = curaz		#update srtconfig.json with new azimuth and altitude values
+		station['config']['azal']['azimuth'] = curaz		# update srtconfig.json with new azimuth and altitude values
 		station['config']['azal']['altitude'] = cural
 		
-		with open('srtconfig.json','w') as f:				#rewrite srtconfig.json with new information
+		with open('srtconfig.json','w') as f:				# rewrite srtconfig.json with new information
 			json.dump(station,f,indent=4)
 
-		### closes socket ###
+		### close socket ###
 
 		try:
 			self.sock.shutdown(socket.SHUT_RDWR)
 			self.sock.close()
 		except OSError:
-			pass  #server already closed socket
+			pass  				# server already closed socket
 
-	##############################################################
+	
 	'''
 	Method that takes a source from the config sources list and moves the station to the position of that source.
+
 	:param sourcename: name of source as labelled in the srtconfig.json sources list
 	:return:
 	'''
@@ -124,32 +128,33 @@ class CommandStation:
 
 		### get current station information ###
 
-		with open('srtconfig.json') as f:								#get station data from config
+		with open('srtconfig.json') as f:								# get station data from config
 			station = json.load(f)
 
-		source = station['config']['sources'][sourcename]				#look up source by name
-		source = SkyCoord(source['ras'],source['dec'],frame = 'icrs')	#convert source into astropy SkyCoord object for coord transformation
+		source = station['config']['sources'][sourcename]				# look up source by name
+		source = SkyCoord(source['ras'],source['dec'],frame = 'icrs')	# convert source into astropy SkyCoord object for coord transformation
 
-		location = station['config']['station']							#look up station location data
-		location = EarthLocation(lat = location['lat'], lon = location['lon'], height = location['height']) 	#convert location into astropy EarthLocation
+		location = station['config']['station']																	# look up station location data
+		location = EarthLocation(lat = location['lat'], lon = location['lon'], height = location['height']) 	# convert location into astropy EarthLocation
 
 		c = ntplib.NTPClient()
-		ntptime = c.request('ntp.carleton.edu',version = 4)				#get current time from Carleton's NTP server
-		unixtime = ntptime.tx_time - 18000								#convert ntp time to unix time
+		ntptime = c.request('ntp.carleton.edu',version = 4)				# get current time from Carleton's NTP server
+		unixtime = ntptime.tx_time - 18000								# convert ntp time to unix time
 
-		observingtime = Time(unixtime,format = 'unix')					#create astropy Time object using converted ntp time
+		observingtime = Time(unixtime,format = 'unix')					# create astropy Time object using converted ntp time
 
-		azalt = AltAz(location = location, obstime = observingtime)		#create AltAz reference frame
+		azalt = AltAz(location = location, obstime = observingtime)		# create AltAz reference frame
 
-		source = source.transform_to(azalt)								#transform source from ras/dec coords to az/alt coords
+		source = source.transform_to(azalt)								# transform source from ras/dec coords to az/alt coords
 
 		### command station with az/alt coords ###
 
 		self.movebyazal(source.az,source.alt)
 
-	##################################################################
+	
 	'''
 	Method that commands the station to take a single power reading at a single frequency.
+
 	:para freq: frequency 
 	:return power: float containing the power reading at frequency
 	'''
@@ -157,14 +162,14 @@ class CommandStation:
 
 		### prepare command message ###
 
-		j = int((freq / 0.04) + 0.5)			#calculate integer value j necessary for conversion to byte sections to feed stamp controller
-		atten = 0								#currently no means of setting attenuation constant, 0 is default of no attenuation
+		j = int((freq / 0.04) + 0.5)			# calculate integer value j necessary for conversion to byte sections to feed stamp controller
+		atten = 0								# currently no means of setting attenuation constant, 0 is default of no attenuation
 		b8 = atten
-		b9 = j & 0x3f							#upper 3 bits of j
-		b10 = (j >> 6) & 0xff					#next byte of j
-		b11 = (j >> 14) & 0xff					#lower byte of j
+		b9 = j & 0x3f							# upper 3 bits of j
+		b10 = (j >> 6) & 0xff					# next byte of j
+		b11 = (j >> 14) & 0xff					# lower byte of j
 
-		message = []							#message must be constructed as a list to properly send command
+		message = []							# message must be constructed as a list to properly send command
 		message.append('freq')
 		message.append(b11)
 		message.append(b10)
@@ -179,8 +184,8 @@ class CommandStation:
 
 		# currently deprecated
 		'''
-		#receive response from stamp via serial to ethernet converter
-		#stamp sends back one thing at a time, so a loop is necessary to receive all the data
+		# receive response from stamp via serial to ethernet converter
+		# stamp sends back one thing at a time, so a loop is necessary to receive all the data
 		response = []
 		for x in range(300):
 			try:
@@ -188,29 +193,29 @@ class CommandStation:
 				print(j.decode('ascii').strip())
 				response.append(j.decode('ascii').strip())
 			except OSError:
-				break											#TCP socket times out after stamp stops sending
+				break											# TCP socket times out after stamp stops sending
 		'''
 
-		response = self.sock.recv(DEFAULT_BUFFER_SIZE).decode('ascii').strip().split()		#receive and format response data
+		response = self.sock.recv(DEFAULT_BUFFER_SIZE).decode('ascii').strip().split()		# receive and format response data
 
-		#close the socket if it hasn't been already closed by the server
-		#only closes if no more data is being received
+		### close socket ###
+
 		try:
 			self.sock.shutdown(socket.SHUT_RDWR)
 			self.sock.close()
 		except OSError:
-			pass  #server already closed socket
+			pass  				# server already closed socket
 
 		### calculate power from received values and return ###
 
 		w2 = int(response[0])
 		w1 = int(response[1]) * 256 + int(response[2])
 		power = 1e6 * (w2 / w1)
-		gaincor = 1							#currently no means of setting gain correction. likely will be set to 1 anyway
+		gaincor = 1							# currently no means of setting gain correction. likely will be set to 1 anyway
 		if atten == 0:
 			power = gaincor * power
 		else:
-			power = gaincor * power * 10	#number 10 used in java code for unknown reasons
+			power = gaincor * power * 10	# number 10 used in java code for unknown reasons
 
 		return power
 
@@ -221,27 +226,27 @@ def main():
 	command = CommandStation()
 
 	print('test: azimuth only, small increase/decrease, large increase/decrease')
-	command.movebyazal(200,90)			#small azimuth increase
-	command.movebyazal(180,90)			#small azimuth decrease
-	command.movebyazal(360,90)			#large azimuth increase
-	command.movebyazal(180,90)			#large azimuth decrease
+	command.movebyazal(200,90)			# small azimuth increase
+	command.movebyazal(180,90)			# small azimuth decrease
+	command.movebyazal(360,90)			# large azimuth increase
+	command.movebyazal(180,90)			# large azimuth decrease
 
 	print('test: altitude only, small increase/decrease, large increase/decrease')
-	command.movebyazal(180,120)			#small altitude increase
-	command.movebyazal(180,90)			#small altitude decrease
-	command.movebyazal(180,180)			#large altitude increase
-	command.movebyazal(180,90)			#large altitude decrease
+	command.movebyazal(180,120)			# small altitude increase
+	command.movebyazal(180,90)			# small altitude decrease
+	command.movebyazal(180,180)			# large altitude increase
+	command.movebyazal(180,90)			# large altitude decrease
 
 	print('test: azimuth and altitude same direction, small increase/decrease, large increase/decrease')
-	command.movebyazal(200,120)			#small azimuth and altitude increase
-	command.movebyazal(180,90)			#small azimuth and altitude decrease
-	command.movebyazal(360,180)			#large azimuth and altitude increase
-	command.movebyazal(180,90)			#large azimuth and altitude decrease
+	command.movebyazal(200,120)			# small azimuth and altitude increase
+	command.movebyazal(180,90)			# small azimuth and altitude decrease
+	command.movebyazal(360,180)			# large azimuth and altitude increase
+	command.movebyazal(180,90)			# large azimuth and altitude decrease
 
 	print('test: azimuth and altitude opposite direction, small increase/decrease, large increase/decrease')
-	command.movebyazal(200,30)			#small azimuth increase and small altitude decrease
-	command.movebyazal(180,90)			#small azimuth decrease and small altitude increase
-	command.movebyazal(360,0)			#large azimuth increase and large altitude decrease
-	command.movebyazal(180,90)			#large azimuth decrease and large altitude increase
+	command.movebyazal(200,30)			# small azimuth increase and small altitude decrease
+	command.movebyazal(180,90)			# small azimuth decrease and small altitude increase
+	command.movebyazal(360,0)			# large azimuth increase and large altitude decrease
+	command.movebyazal(180,90)			# large azimuth decrease and large altitude increase
 
-main()
+#main()
--- backend/Scan.py
+++ backend/Scan.py
@@ -1,13 +1,15 @@
 '''
-A class that performs various types of scans by looping the methods
-found in CommandStation.py.
+A class that performs tracking and drift scans at
+particular frequencies and of the whole station spectrum.
 
 Author: Nathan Rowley
+Date: June 2018
 '''
 
 import CommandStation
 import json
-from astropy.table import Table
+import ntplib
+from astropy.table import Table, Column, vstack
 
 class Scan:
 
@@ -17,85 +19,185 @@ class Scan:
 	'''
 	Method to take a single data point at a single frequency for a single source.
 
-	:param source: name of source to measure
-	:param freq: frequency at which to measure in MHz
-	:return power: power measurement in degrees Kelvin
+	:param pos: list containing azimuth and altitude of scan position
+	:param freq: frequency in MHz at which to measure
+	:return scan: astropy Table object containing a single power measurement
 	'''
-	def singlescan(self,source,freq) -> float:
+	def singlescan(self, pos, freq) -> Table:
 
-		self.station.movebysource(source)		# point station at source
+		self.station.movebyazal(pos[0], pos[1])		# move station to scan position
 
-		power = self.station.readpower(freq)	# read power at frequency freq
+		scan = self.station.readpower(freq)			# read power at frequency freq
+		curtime = getcurrenttime()					# get time of scan
+		
+		freqcol = Column([freq], name = 'frequency', unit = 'MHz')	# convert lists to astropy Column objects
+		powcol  = Column([scan], name = 'temperature', unit = 'K')
+		timecol = Column([curtime], name = 'time', unit = 's')
 
-		return power
+		scan = Table((freqcol, powcol, timecol))					# create astropy Table object containing scan data
+
+		return scan
 
 	'''
 	Method to take data points across the whole station spectrum for a single source.
 
-	:param source: name of source to measure
-	:return spectrum: an astropy Table containing a spectrum measurement
+	:param source: list containing azimuth and altitude of scan position
+	:return spectrum: astropy Table object containing a single spectrum measurement
 	'''
-	def singlespectrum(self,source) -> Table:
+	def singlespectrum(self, pos) -> Table:
 
-		self.station.movebysource(source)		# point station at source
+		with open('srtconfig.json') as f:			# load in config data
+			stationdata = json.load(f)
 
-		with open('srtconfig.json') as f:		# load in config data
-			station = json.load(f)
+		lowf  = stationdata['config']['freqrange']['lower']		# set spectrum range
+		highf = stationdata['config']['freqrange']['upper']
 
-		lowf  = station['config']['freqrange']['lower']		# set spectrum range
-		highf = station['config']['freqrange']['upper']
+		scan     = Table()										# intialize astropy Table objects to store spectrum data
+		spectrum = Table()
 
-		spectrumfreq = []									# create lists to store spectrum data
-		spectrumpow  = []
+		curtime = getcurrenttime()								# get time of spectrum scan
 
 		for freq in range(lowf,highf,0.04):						# sweep through frequencies in range, taking 40 kHz steps
 
-			spectrumfreq.append(freq)							# record frequency
+			scan = singlescan(pos, freq)						# get scan at current frequency
 
-			spectrumpow.append(self.station.readpower(freq))	# record measured power
+			scan['time'][1] = curtime							# set time to current time
 
-		spectrum = Table([spectrumfreq, spectrumpow], names = ('frequency - MHz', 'power - K'))	# convert spectrum data to an astropy Table
+			spectrum = vstack([spectrum, scan])					# append scan to the spectrum
 
 		return spectrum
 
 	'''
-	Method to track and take data points on a single source at a single frequency for a specified amount of time.
+	Method to track and take data points on a single source at a single frequency until a specified time.
 
 	:param source: name of source to track and measure
 	:param freq: frequency at which to measure in MHz
-	:param time: amount of time to track in seconds
-	:return :
+	:param time: unix time at which to stop scanning
+	:return scan: astropy Table object containing a scan of source over time
 	'''
-	def trackscan(self, source, freq, time):
+	def trackscan(self, source, freq, time) -> Table:
 
+		scan = Table()						# initialize data Table
+
+		curtime = getcurrenttime()			# get start time of scan
+
+		while curtime < time:				# continue scanning until the current time is past the end time
+
+			pos = getsourceazal(source)		# get azal coords for source
+
+			scan = vstack([scan, singlescan(pos, freq)])		# take reading and append to the scan
+
+			curtime = getcurrenttime()		# update current time
+
+		return scan
 
 	'''
-	Method to track and take data points on a single source across the whole station spectrum for a specific amount of time.
+	Method to track and take data points on a single source across the whole station spectrum until a specific time.
 
 	:param source: name of source to track and measure
-	:param time: amount of time to track in seconds
-	:return :
+	:param time: unix time at which to stop scanning
+	:return spectrum: astropy Table object containing a scan of the source's spectrum over time
 	'''
-	def trackspectrum(self, source, time):
+	def trackspectrum(self, source, time) -> Table:
 
+		curtime = getcurrenttime()			# get start time of scan
+
+		spectrum = Table()					# initialize data Table
+
+		while curtime < time:				# continue scanning until the current time is past the end time
+
+			pos = getsourceazal(source)		# get azal coords for source
+
+			spectrum = vstack([spectrum, singlespectrum(pos)])	# take spectrum and append to the scan
+
+			curtime = getcurrenttime()		# update current time
+
+		return spectrum
 
 	'''
-	Method to take a drift scan at a single frequency for a specific amount of time.
+	Method to take a drift scan at a single frequency until a specific time.
 
-	:param pos: tuple containing azimuth and altitude of drift position
+	:param pos: list containing azimuth and altitude of drift position
 	:param freq: frequency at which to measure in MHz
-	:param time: amount of time to take data in seconds
-	:return :
+	:param time: unix time at which to stop scanning
+	:return scan: astropy Table object containing a drift scan
 	'''
-	def driftscan(self, pos, freq, time):
+	def driftscan(self, pos, freq, time) -> Table:
 
+		scan = Table()						# initialize data Table
+
+		curtime = getcurrenttime()			# get start time of scan
+
+		while curtime < time:				# continue scanning until the current time is past the end time
+
+			scan = vstack([scan, singlescan(pos, freq)])		# take reading and append to the scan
+
+			curtime = getcurrenttime()		# update current time
+
+		return scan
 
 	'''
-	Method to take a drift scan across the whole station spectrum for a specific amount of time.
+	Method to take a drift scan across the whole station spectrum until a specific time.
 
 	:param pos: tuple containing azimuth and altitude of drift position
-	:param time: amount fo time to take data in seconds
-	:return :
+	:param time: unix time at which to stop scanning
+	:return spectrum: astropy table object containing a drift scan of spectrum over time
 	'''
-	def driftspectrum(self, pos, time):
+	def driftspectrum(self, pos, time) -> Table:
 
+		curtime = getcurrenttime()			# get start time of scan
+
+		spectrum = Table()					# initialize data Table
+
+		while curtime < time:				# continue scanning until the current time is past the end time
+
+			spectrum = vstack([spectrum, singlespectrum(pos)])	# take spectrum and append to the scan
+
+			curtime = getcurrenttime()		# update current time
+
+		return spectrum
+
+	'''
+	Helper method to get the current time from an NTP server.
+
+	:return unixtime: current unix time
+	'''
+	def getcurrenttime(self):
+
+		c = ntplib.NTPClient()
+		ntptime = c.request('ntp.carleton.edu',version = 4)				# get current time from Carleton's NTP server
+		unixtime = ntptime.tx_time - 18000								# convert ntp time to unix time
+
+		return unixtime
+
+	'''
+	Helper method to get the azimuth and altitude of a source.
+
+	:param source: name of source
+	:return pos: list containing azimuth and altitude of source
+	'''
+	def getsourceazal(self, source):
+
+		with open('srtconfig.json') as f:								# get station data from config
+			stationdata = json.load(f)
+
+		source = stationdata['config']['sources'][sourcename]			# look up source by name
+		source = SkyCoord(source['ras'],source['dec'],frame = 'icrs')	# convert source into astropy SkyCoord object for coord transformation
+
+		location = stationdata['config']['station']																# look up station location data
+		location = EarthLocation(lat = location['lat'], lon = location['lon'], height = location['height']) 	# convert location into astropy EarthLocation
+
+		unixtime = getcurrenttime()										# get curent time to establish AltAz reference frame
+
+		observingtime = Time(unixtime,format = 'unix')					# create astropy Time object using converted ntp time
+
+		azalt = AltAz(location = location, obstime = observingtime)		# create AltAz reference frame
+
+		source = source.transform_to(azalt)								# transform source from ras/dec coords to az/alt coords
+
+		pos = []								# initialize position list
+
+		pos.append(source.az)					# fill position list with azimuth and altitude
+		pos.append(source.al)
+
+		return pos
